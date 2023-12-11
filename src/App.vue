3fd5a9c8c431a1d077296bcf730065f7121291c7
mariano <marianoarias98@gmail.com>
2023-12-11 13:31:25 -0300

    feat: add time from now buttons

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,79 @@
 <template>
   <div class="template">
     <div class="card p-4" v-if="showSelectTime">
-      <h5 class="text-center">Select the time</h5>
-      <div class="time d-flex justify-content-between mb-3">
-        <input type="number" min="0" max="23" v-model="targetHour" class="no-spinners" maxlength="2"/>
-        <span>:</span>
-        <input type="number" min="0" max="59" v-model="targetMinute" class="no-spinners" maxlength="2"/>
+      <h4 class="text-center mb-3">Select time</h4>
+      <div class="d-flex justify-content-between align-items-center gap-2 mb-3">
+        <p class="mb-0">From now:</p>
+        <div class="d-flex gap-1">
+          <button class="btn btn-outline-secondary" @click="addMinutes(30)">
+            +30 m
+          </button>
+          <button class="btn btn-outline-secondary" @click="addMinutes(60)">
+            +60 m
+          </button>
+          <button class="btn btn-outline-secondary" @click="addMinutes(90)">
+            +90 m
+          </button>
+        </div>
       </div>
-      <button class="btn btn-outline-primary" @click="handleInput">Start</button>
+      <div class="time d-flex justify-content-between mb-3">
+        <input
+          type="number"
+          min="0"
+          max="23"
+          v-model="targetHour"
+          class="no-spinners"
+          maxlength="2"
+        />
+        <span>:</span>
+        <input
+          type="number"
+          min="0"
+          max="59"
+          v-model="targetMinute"
+          class="no-spinners"
+          maxlength="2"
+        />
+      </div>
+      <button class="btn btn-outline-primary" @click="handleInput">
+        Start
+      </button>
     </div>
     <div class="timer-div" v-else>
       <h2>Remaining time...</h2>
-      <p class="timer">{{ countdownRef.hours }}:{{ countdownRef.minutes }}:{{ countdownRef.seconds }}</p>
-      <h4 class="text-end">Target Time: {{ targetHour }}:{{ targetMinute }} hs</h4>
+      <p class="timer">
+        {{ countdownRef.hours }}:{{ countdownRef.minutes }}:{{
+          countdownRef.seconds
+        }}
+      </p>
+      <h4 class="text-end">
+        Target Time: {{ targetHour }}:{{ targetMinute }} hs
+      </h4>
     </div>
   </div>
 </template>
 
 <script setup>
-import { onMounted, ref, watch } from 'vue';
+import { onMounted, ref, watch } from "vue";
 
 const defaultTargetTime = new Date();
 defaultTargetTime.setMinutes(defaultTargetTime.getMinutes() + 90);
 
-const targetHour = ref(localStorage.getItem('targetHour') || defaultTargetTime.getHours());
-const targetMinute = ref(localStorage.getItem('targetMinute') || defaultTargetTime.getMinutes());
+const targetHour = ref(
+  localStorage.getItem("targetHour") || defaultTargetTime.getHours()
+);
+const targetMinute = ref(
+  localStorage.getItem("targetMinute") || defaultTargetTime.getMinutes()
+);
 
-const showSelectTime = ref(localStorage.getItem('showSelectTime') === 'true' || true);
+const showSelectTime = ref(
+  localStorage.getItem("showSelectTime") === "true" || true
+);
 
 const countdown = {
   hours: 0,
   minutes: 0,
-  seconds: 0
+  seconds: 0,
 };
 
 const countdownRef = ref(countdown);
@@ -49,9 +91,16 @@ const startCountdown = () => {
 
     const diff = targetTime - now;
 
-    countdownRef.value.seconds = String(Math.floor(diff / 1000) % 60).padStart(2, '0');
-    countdownRef.value.minutes = String(Math.floor(diff / 1000 / 60) % 60).padStart(2, '0');
-    countdownRef.value.hours = String(Math.floor(diff / 1000 / 60 / 60) % 24).padStart(2, '0');
+    countdownRef.value.seconds = String(Math.floor(diff / 1000) % 60).padStart(
+      2,
+      "0"
+    );
+    countdownRef.value.minutes = String(
+      Math.floor(diff / 1000 / 60) % 60
+    ).padStart(2, "0");
+    countdownRef.value.hours = String(
+      Math.floor(diff / 1000 / 60 / 60) % 24
+    ).padStart(2, "0");
 
     // Update document title with the timer text
     document.title = `${countdownRef.value.hours}:${countdownRef.value.minutes}:${countdownRef.value.seconds} - Remaining time`;
@@ -75,14 +124,14 @@ onMounted(() => {
 
 const handleInput = () => {
   showSelectTime.value = !showSelectTime.value;
-  localStorage.setItem('showSelectTime', showSelectTime.value);
+  localStorage.setItem("showSelectTime", showSelectTime.value);
 };
 
 // Observa los cambios en targetHour, targetMinute y showSelectTime y almacena los valores en localStorage
 watch([targetHour, targetMinute, showSelectTime], () => {
-  localStorage.setItem('targetHour', targetHour.value);
-  localStorage.setItem('targetMinute', targetMinute.value);
-  localStorage.setItem('showSelectTime', showSelectTime.value);
+  localStorage.setItem("targetHour", targetHour.value);
+  localStorage.setItem("targetMinute", targetMinute.value);
+  localStorage.setItem("showSelectTime", showSelectTime.value);
 });
 
 // Verificar el valor inicial de showSelectTime al cargar la página
@@ -90,6 +139,12 @@ if (showSelectTime.value === false) {
   showSelectTime.value = true;
 }
 
+const addMinutes = (minutes) => {
+  const now = new Date();
+  now.setMinutes(now.getMinutes() + minutes);
+  targetHour.value = now.getHours();
+  targetMinute.value = now.getMinutes();
+};
 </script>
 
 <style scoped>
@@ -106,7 +161,8 @@ if (showSelectTime.value === false) {
   flex-direction: column;
   height: 100vh;
   font-family: sans-serif;
-  background-image: linear-gradient(to top, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0)), url(./assets/bg.jpg);
+  background-image: linear-gradient(to top, rgba(0, 0, 0, 1), rgba(0, 0, 0, 0)),
+    url(./assets/bg.jpg);
   background-size: cover;
   color: white;
   box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.8);
@@ -131,7 +187,7 @@ input {
   border-radius: 10px;
 }
 
-input:focus{
+input:focus {
   outline: 1px solid rgb(31, 128, 255);
 }
 
